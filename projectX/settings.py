19292359f06d4995b5b5f23f8a95277c1e3a3b1c
Reddy Tintaya <aaabeeelooon@gmail.com>
2021-09-04 17:41:53 -0400

    basic configuratio for mysql

--- projectX/settings.py
+++ projectX/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "apps.news",
     "apps.sources",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -56,13 +57,14 @@ TEMPLATES = [
 WSGI_APPLICATION = "projectX.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "projectX",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
@@ -104,3 +106,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+ALLOWED_HOSTS = ["*"]
